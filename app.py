258047dd2fb6363fc18d991e85eacb289cbb32f5
Vandhithapushpala <121685587+Vandhithapushpala@users.noreply.github.com>
2023-02-05 13:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def Predict_level_of_pollution(data: Pollutants):
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='127.0.0.1', port=8000)
 
-# uvicorn app:app --reload
+uvicorn app:app --reload
